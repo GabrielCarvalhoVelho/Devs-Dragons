af500cadcd06c5dfe42570b30b468854e4d98fb9
GabrielCarvalhoV <serumanovelho@gmail.com>
2023-04-22 20:13:42 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { Personagem } from "./modules/personagem.js"
 import { PersonagemView } from "./components/personagem-view.js"
+import { Mago } from "./modules/mago.js"
 
 const personagemPedrinho = new Personagem('Pedrinho', 5, 'Mago')
 const personagemJose = new Personagem('Jose', 3, 'Arqueiro')
@@ -8,4 +9,4 @@ const personagens = [personagemPedrinho, personagemJose]
 
 new PersonagemView(personagens).render()
 
-console.log(personagemJose.vida)
+console.log(new Mago('gelo', 7, 9))
